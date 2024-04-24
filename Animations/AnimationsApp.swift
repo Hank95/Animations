edd40cd6218513f97afcb37d1e9027abfd0e9bdc
Henry Pendleton <hhpendleton@gmail.com>
2024-04-24 14:26:49 -0400

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Henry Pendleton on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
