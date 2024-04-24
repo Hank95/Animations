fbef881580ec69f9a5ed9916de8c94ac21582b14
Henry Pendleton <hhpendleton@gmail.com>
2024-04-24 14:53:20 -0400

    playing with animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+
+            Spacer()
+
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount)
         }
-        .padding()
     }
 }
 
